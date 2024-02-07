bdb8a1a6e1655a73e906c7aaac189e272ac96614
VitorDanielA <vdanieldev1@gmail.com>
2024-02-07 00:29:38 -0300

    Criação da lógica para salvar no backend

---
+++ chat-tempo-real/src/pages/TelaChat.tsx
@@ -0,0 +1,7 @@
+export default function TelaChat(){
+    return(
+        <div>
+            
+        </div>
+    )
+}
--- chat-tempo-real/src/pages/TelaCriacaoUsuario.tsx
+++ chat-tempo-real/src/pages/TelaCriacaoUsuario.tsx
@@ -4,14 +4,130 @@ import { Link } from 'react-router-dom';
 import { useState } from 'react';
 import EyeFill from './../assets/eye-fill.svg';
 import EyeSlash from './../assets/eye-slash.svg';
+import Alert from 'react-bootstrap/Alert';
 
 export default function TelaCriacaoUsuario(){
 
+    class Usuario{
+        login:string = '';
+        senha:string = '';
+        email:string = '';
+    }
+
     const [mostrarSenha, setMostrarSenha] = useState(false);
+    const [mostrarSenhaVerificar, setMostrarSenhaVerificar] = useState(false);
+    const [usuario, setUsuario] = useState(new Usuario());
+    const [mostrarAlerta, setMostrarAlerta] = useState(false);
+    const [mostrarAlertaSucesso, setMostrarAlertaSucesso] = useState(false);
+    const [verificarSenha, setVerificarSenha] = useState('');
 
     const toggleMostrarSenha = () => {
         setMostrarSenha(!mostrarSenha);
     }
+
+    const toggleMostrarSenhaVerificar = () => {
+        setMostrarSenhaVerificar(!mostrarSenhaVerificar);
+    }
+
+    const handleChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { value } = e.target;
+        setUsuario(prevState => ({
+            ...prevState,
+            email: value
+        }));
+    }
+
+    const handleChangeSenha = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { value } = e.target;
+        setUsuario(prevState => ({
+            ...prevState,
+            senha: value
+        }));
+    }
+    
+    const handleChangeLogin = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { value } = e.target;
+        setUsuario(prevState => ({
+            ...prevState,
+            login: value
+        }));
+    }
+
+    const isLoginValido = (login:string) =>{
+        return login.length > 4;
+    }
+
+    const isEmailValido = (email:string) => {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
+    }
+
+    const isSenhaValida = (senha:string) => {
+        const letterRegex = /[a-zA-Z]/;
+        const numberRegex = /[0-9]/;
+        return letterRegex.test(senha) && numberRegex.test(senha);
+    }
+
+    function criarUsuario(){
+
+        if(usuario.email === '' || usuario.login === '' || usuario.senha === '' || usuario.senha != verificarSenha){
+            setMostrarAlerta(true);
+            setMostrarAlertaSucesso(false);
+            return;
+        }
+
+        if(!isEmailValido(usuario.email) || !isSenhaValida(usuario.senha) || !isLoginValido(usuario.login)){
+            setMostrarAlerta(true);
+            setMostrarAlertaSucesso(false);
+            return
+        }
+
+        fetch("http://localhost:5000/usuarios", {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json',
+            },
+            body: JSON.stringify(usuario),
+        })
+            .then((resp) => resp.json())
+            .then((data) => {
+                console.log(data)
+                setUsuario(new Usuario());
+                setVerificarSenha('');
+                setMostrarAlertaSucesso(true);
+                setMostrarAlerta(false);
+            })
+            .catch(err => console.log(err))
+            setMostrarAlertaSucesso(true);
+            setMostrarAlerta(false);
+    }
+
+    const alertaErro = () => {
+        if (mostrarAlerta) {
+            return (
+              <Alert variant="danger" className='my-3' onClose={() => setMostrarAlerta(false)} dismissible>
+                <Alert.Heading>Houve um erro no cadastro</Alert.Heading>
+                <label>
+                  Tente inserir as informações corretamente! (Login com mais de 4 caracteres, senha com números e letras além de iguais nos 2 campos, e email com formato válido).
+                </label>
+              </Alert>
+            );
+        }
+    }
+
+    const alertaSucesso = () => {
+        if (mostrarAlertaSucesso) {
+            return (
+              <Alert variant="success" className='my-3' onClose={() => setMostrarAlertaSucesso(false)} dismissible>
+                <Alert.Heading>Conta criada com sucesso!</Alert.Heading>
+                <label>
+                  Cadastro na nossa plataforma feita com sucesso! Entre na tela de Login para acessar o WebChat.
+                </label>
+              </Alert>
+            );
+        }
+    }
+
 
     return(
         <div className={`${styles.body}`}>
@@ -25,17 +141,23 @@ export default function TelaCriacaoUsuario(){
                     type="text" 
                     className="form-control" 
                     placeholder='Seu Nome de Usuário'
+                    value={usuario.login}
+                    onChange={handleChangeLogin}
                 />
                 <input 
                     type="email" 
                     className="form-control my-4" 
                     placeholder='Seu Email'
+                    value={usuario.email}
+                    onChange={handleChangeEmail}
                 />
 
                 <div className='mb-3'>
                     <input 
                         type={mostrarSenha ? 'text' : 'password'} 
                         placeholder='Sua Senha'
+                        value={usuario.senha}
+                        onChange={handleChangeSenha}
                     />
                     
                     <i onClick={toggleMostrarSenha}>
@@ -46,15 +168,19 @@ export default function TelaCriacaoUsuario(){
                 
                 <div className='mt-4'>
                     <input 
-                        type={mostrarSenha ? 'text' : 'password'} 
+                        type={mostrarSenhaVerificar ? 'text' : 'password'} 
                         placeholder='Confirmar Senha'
+                        onChange={(e) => setVerificarSenha(e.target.value)}
+                        value={verificarSenha}
                     />
                     
-                    <i onClick={toggleMostrarSenha}>
-                        {mostrarSenha ? <img src={EyeFill} alt="ocultar senha" /> : <img src={EyeSlash} alt="mostrar senha" />}
+                    <i onClick={toggleMostrarSenhaVerificar}>
+                        {mostrarSenhaVerificar ? <img src={EyeFill} alt="ocultar senha" /> : <img src={EyeSlash} alt="mostrar senha" />}
                     </i>
                 </div>
-                <button className={`${styles.btnInscrever} btn mt-3`}>Inscrever</button>
+                <button className={`${styles.btnInscrever} btn mt-3`} onClick={() => { criarUsuario(); setUsuario(new Usuario()); }}>Inscrever</button>
+                {alertaErro()}
+                {alertaSucesso()}
                 <p className='text-center mt-4'>Você já têm uma conta?<Link to={'/login'}> Entrar</Link></p>
             </div>
         </div>
